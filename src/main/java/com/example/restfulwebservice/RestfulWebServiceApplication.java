62b62baa39e175a7f515f3df7d563d3624f6eaf0
donghyeon0725 <ehdgus5015@gmail.com>
2021-04-20 14:13:14 +0900

    git add 다국어 처리

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -1,16 +1,23 @@
 package com.example.restfulwebservice;
 
+import net.rakugakibox.util.YamlResourceBundle;
 import org.apache.tomcat.util.descriptor.LocalResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.naming.spi.Resolver;
 import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -22,8 +29,29 @@ public class RestfulWebServiceApplication {
     @Bean
     public LocaleResolver localResolver() {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        // 언어&국가정보가 없는 경우 미국으로 인식하도록 설정
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
     }
 
+    @Bean("messageSource")
+    public MessageSource messageSource(
+            @Value("${spring.messages.basename}") String basename,
+            @Value("${spring.messages.encoding}") String encoding
+    ) {
+        YamlMessageSource ms = new YamlMessageSource();
+        ms.setBasename(basename);
+        ms.setDefaultEncoding(encoding);
+        ms.setAlwaysUseMessageFormat(true);
+        ms.setUseCodeAsDefaultMessage(true);
+        ms.setFallbackToSystemLocale(true);
+        return ms;
+    }
+
+    class YamlMessageSource extends ResourceBundleMessageSource {
+        @Override
+        protected ResourceBundle doGetBundle(String basename, Locale locale) throws MissingResourceException {
+            return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
+        }
+    }
 }
--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.example.restfulwebservice.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 @RestController
 public class HelloWorldController {
@@ -32,9 +37,10 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello world, %s", name));
     }
 
-    /*@GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) { //
         System.out.println(locale);
         return messageSource.getMessage("greeting.message", null, locale);
-    }*/
+    }
+
 }
